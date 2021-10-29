ded47ac739dc9b7a3e5df60638e9ed3fcbd2d8d0
Mahmud Chamtooni <38923023+MahmudSuleman@users.noreply.github.com>
2021-10-29 22:52:58 +0000

    verify token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'https://nanti-flutter-web-laravel-api.herokuapp.com/*'
     ];
 }
